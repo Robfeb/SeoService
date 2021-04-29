6d77c2f4ba66f9eafddeaadf87535827b61779ee
Roberto Febrer <febrer@gmail.com>
2021-04-29 09:32:33 +0200

    * appsettings.json: * SEOService.csproj: * SeoController.cs:
    * Startup.cs: Add Simple Script to fetch data to server

--- SEOService/Controllers/SeoController.cs
+++ SEOService/Controllers/SeoController.cs
@@ -1,34 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Extensions.Configuration;
 
 namespace SEOService.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+
     public class SeoController : Controller
     {
+
         private Dictionary<int, string> UrlList;
         const string tokenCustomer = "123456";
-        public SeoController()
+        IConfiguration Configuration;
+        public SeoController(IConfiguration configuration)
         {
-            UrlList = new Dictionary<int,string>();
+            Configuration = configuration;
+            UrlList = new Dictionary<int, string>();
             for (int i = 0; i < 20; i++)
             {
                 var link = $"https://www.link{i}.com/link{i}";
                 var text = $"link{i}";
-                UrlList.Add(i,$"<a href='{link}'>{text}</a>");
+                UrlList.Add(i, $"<a href='{link}'>{text}</a>");
             }
         }
         [HttpGet]
         public IActionResult Index(string token)
         {
-            string urlsCustomer=string.Empty;
+            return GetLinks(token);
+        }
+
+        [HttpGet]
+        [Route("scriptWithCode.js")]
+        public IActionResult ScriptCode()
+        {
+            HttpContext.Response.Headers.Add("Content-Type", "application/json");
+            var URL = Configuration["UrlBase"].ToString();
+            
+            string scriptContent = "var getData = fetch('"+URL+ "?token=123456').then(function(response) {return response.text()}).then(function(body) {var divLinks = document.createElement('div');divLinks.id = 'div_links';divLinks.innerHTML = body;document.body.appendChild(divLinks);return console.log(body);});";
+            //scriptContent = "const getData = fetch('{URL}').then(response => response.json).then(jsonObject => console.log(jsonObject));";
+
+            return Content(scriptContent);
+        }
+
+        [HttpGet]
+        [Route("script.js")]
+        public IActionResult Script()
+        {
+            HttpContext.Response.Headers.Add("Content-Type", "application/json");
+            var URL = Configuration["UrlBase"].ToString();
+
+            string scriptContent = "var key='000000'; if (typeof token !== 'undefined') {key=token;}var getData = fetch('" + URL + "?token='+key).then(function(response) {return response.text()}).then(function(body) {var divLinks = document.createElement('div');divLinks.id = 'div_links';divLinks.innerHTML = body;document.body.appendChild(divLinks);return console.log(body);});";
+            //scriptContent = "const getData = fetch('{URL}').then(response => response.json).then(jsonObject => console.log(jsonObject));";
+
+            return Content(scriptContent);
+        }
+        private IActionResult GetLinks(string token)
+        {
+            string urlsCustomer = string.Empty;
             if (isAuthenticated(token))
             {
                 Random rnd = new Random();
-                int customerRandomNumber = rnd.Next(0, UrlList.Count-2);
+                int customerRandomNumber = rnd.Next(0, UrlList.Count - 2);
                 for (int i = customerRandomNumber; i < UrlList.Count; i++)
                 {
                     urlsCustomer += UrlList[i];
@@ -36,7 +71,6 @@ namespace SEOService.Controllers
             }
             return Content(urlsCustomer);
         }
-
         private bool isAuthenticated(string token)
         {
             return !string.IsNullOrEmpty(token) && tokenCustomer.Equals(token);
--- SEOService/Startup.cs
+++ SEOService/Startup.cs
@@ -4,12 +4,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Swagger;
 namespace SEOService
 {
 
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -30,6 +30,12 @@ namespace SEOService
                     Description = "Description for the API goes here.",
                 });
             });
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,7 +58,8 @@ namespace SEOService
 
             app.UseAuthorization();
 
-           
+            app.UseCors(MyAllowSpecificOrigins);
+          
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
